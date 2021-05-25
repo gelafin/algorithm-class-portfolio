ecb8aec3886091db15c894dc989f46d521d3adb2
gelafin <themarkymarkers@gmail.com>
2021-05-25 13:00:36 -0700

    draft Patternmatch skeleton

---
+++ Patternmatch.py
@@ -0,0 +1,20 @@
+# Author: Mark Mendez
+# Date: 05/25/2021
+# Description: Implementation of a basic pattern matcher
+
+
+def pattermatch(string, p):
+    """
+    Determines whether a given pattern matches a given string
+    :param string: any string to test
+    :param p: string with a pattern; can include literal chars or wildcards:
+              *: matches any char sequence
+              ?: matches any one char
+    :return: True if the pattern matches the string; False otherwise
+    """
+    pass
+
+
+if __name__ == '__main__':
+    # test
+    pass
