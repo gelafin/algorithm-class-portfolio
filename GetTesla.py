1417ff2601cddcfb447e52f1824bd2bf43abd54a
gelafin <themarkymarkers@gmail.com>
2021-05-26 13:14:50 -0700

    finish GetTesla

--- GetTesla.py
+++ GetTesla.py
@@ -3,6 +3,33 @@
 # Description: Implements a shortest-path function with the special constraints of the class assignment
 
 
+import math
+
+
+def get_tesla_neighbors(node, highest_x, highest_y):
+    """
+    Gets all neighbors of a given node which are valid in a "GetTesla" puzzle path
+    :param highest_x: int for the highest valid x-coordinate
+    :param highest_y: int for the highest valid y-coordinate
+    :param node: tuple of ints for x and y coordinates
+    :return: list of tuples of ints for x and y coordinates; list of all valid neighbor nodes
+             If the node has no valid neighbors, returns an empty list
+    """
+    x, y = node
+    list_out = list()
+
+    # add 1 in both directions and return if valid
+    if x + 1 <= highest_x:
+        step_right = (x + 1, y)
+        list_out.append(step_right)
+
+    if y + 1 <= highest_y:
+        step_down = (x, y + 1)
+        list_out.append(step_down)
+
+    return list_out
+
+
 def getTesla(M):
     """
     Calculates the minimum health points needed to traverse the input matrix from top-left to bottom-right, where
@@ -11,8 +38,45 @@ def getTesla(M):
     :param M: list of lists of ints for the matrix with its nodes' values
     :return: int of the minimum health points needed to traverse the matrix
     """
+    # keep a table of results for max hp possible at each node
+    # initialize to guarantee overwrite on first comparison
+    max_hp_table = [[-math.inf for _ in range(len(M[0]))] for _ in range(len(M))]
+    max_hp_table[0][0] = M[0][0]
+
+    # fill the max hp table by visiting every valid node
+    itinerary = {(0, 0)}  # nodes stored as (x, y) coordinates
+    while len(itinerary) > 0:
+        current_node = itinerary.pop()
+        current_x, current_y = current_node
+        current_node_max_hp = max_hp_table[current_x][current_y]
+
+        for neighbor in get_tesla_neighbors(current_node, len(M[0]) - 1, len(M) - 1):
+            neighbor_x, neighbor_y = neighbor
+            neighbor_hp = M[neighbor_y][neighbor_x]
+
+            # calculate max hp to reach each neighbor
+            total_hp_from_current_node = current_node_max_hp + neighbor_hp
+            neighbor_max_hp = max(max_hp_table[neighbor_x][neighbor_y], total_hp_from_current_node)
+            max_hp_table[neighbor_x][neighbor_y] = neighbor_max_hp
+
+            # we need to visit their neighbors too
+            itinerary.add(neighbor)
+
+    # if max hp path is > 0, return 0 bc no health is needed
+    final_max_hp = max_hp_table[len(max_hp_table[0]) - 1][len(max_hp_table) - 1]
+    if final_max_hp > 0:
+        return 0
+    else:
+        # return difference from 1 hp (which is min hp to live)
+        return 1 - final_max_hp
 
 
 if __name__ == '__main__':
     # test
-    pass
+    m = [[-1, -2, 2], [10, -8, 1], [-5, -2, -3]]
+    expected = 2
+    result = getTesla(m)
+    if (result != expected):
+        print('test 1 expected', expected, 'but got', result)
+
+
