edd394eb190de5e632174a910b21552a0cd2e6b6
gelafin <themarkymarkers@gmail.com>
2021-05-25 20:51:07 -0700

    draft skeleton for GetTesla

---
+++ GetTesla.py
@@ -0,0 +1,18 @@
+# Author: Mark Mendez
+# Date: 05/25/2021
+# Description: Implements a shortest-path function with the special constraints of the class assignment
+
+
+def getTesla(M):
+    """
+    Calculates the minimum health points needed to traverse the input matrix from top-left to bottom-right, where
+        positive node values increase health, and
+        negative node values decrease health
+    :param M: list of lists of ints for the matrix with its nodes' values
+    :return: int of the minimum health points needed to traverse the matrix
+    """
+
+
+if __name__ == '__main__':
+    # test
+    pass
